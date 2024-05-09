70c5210dcfecb17dea0cdd195856e96f91782251
Abdel <abdelhakmerabtene@gmail.com>
2024-05-09 17:05:31 +0200

    the NFC is successefully geted

--- app/src/main/java/com/example/espacebenevole/ui/nfc/NFCFragment.kt
+++ app/src/main/java/com/example/espacebenevole/ui/nfc/NFCFragment.kt
@@ -1,56 +1,58 @@
 package com.example.espacebenevole.ui.nfc
 
 import android.content.Context
+import android.nfc.NfcAdapter
+import android.nfc.Tag
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.android.volley.Request
-import com.android.volley.Response
-import com.android.volley.toolbox.JsonArrayRequest
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
-import com.example.espacebenevole.R
-import com.example.espacebenevole.databinding.FragmentHomeBinding
 import com.example.espacebenevole.databinding.FragmentNfcBinding
-import org.json.JSONArray
 
 
-
-class NFCFragment : Fragment() {
+class NFCFragment : Fragment(), NfcAdapter.ReaderCallback {
 
     private var _binding: FragmentNfcBinding? = null
     private val binding get() = _binding!!
+    private var nfcAdapter: NfcAdapter? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentNfcBinding.inflate(inflater, container, false)
+        initNFC()
         return binding.root
     }
 
-    private fun checkAuthenticationAndFetchEvents() {
-        val token = getToken()
-        if (token.isNullOrEmpty()) {
-            redirectToLogin()
-        } else {
-            //fetchAndDisplayEvents(token)
+    private fun initNFC() {
+        nfcAdapter = NfcAdapter.getDefaultAdapter(context)
+        if (nfcAdapter == null) {
+            Toast.makeText(context, "NFC is not supported on this device.", Toast.LENGTH_LONG).show()
+            return
+        }
+        if (!nfcAdapter!!.isEnabled) {
+            Toast.makeText(context, "NFC is disabled.", Toast.LENGTH_LONG).show()
         }
     }
 
-    private fun getToken(): String? {
-        val sharedPreferences = requireActivity().getSharedPreferences("AppPreferences", Context.MODE_PRIVATE)
-        return sharedPreferences.getString("AuthToken", null)
+    override fun onResume() {
+        super.onResume()
+        nfcAdapter?.enableReaderMode(activity, this, NfcAdapter.FLAG_READER_NFC_A or NfcAdapter.FLAG_READER_NFC_B, null)
     }
 
-    private fun redirectToLogin() {
-        Toast.makeText(requireContext(), "Votre session s'est expirée, veuillez vous reconnecter!", Toast.LENGTH_LONG).show()
-        // Handle redirection to login; possibly via Navigation Component or starting a new Activity
+    override fun onPause() {
+        super.onPause()
+        nfcAdapter?.disableReaderMode(activity)
+    }
+
+    override fun onTagDiscovered(tag: Tag?) {
+        val tagInfo = "Tag detected: " + tag?.id?.joinToString("") { "%02x".format(it) }
+        activity?.runOnUiThread {
+            Toast.makeText(context, tagInfo, Toast.LENGTH_LONG).show()
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/espacebenevole/ui/nfc/NFCViewModel.kt
+++ app/src/main/java/com/example/espacebenevole/ui/nfc/NFCViewModel.kt
@@ -4,10 +4,12 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class NFCViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is nfc Fragment"
+    private val _nfcData = MutableLiveData<String>()
+    val nfcData: LiveData<String> = _nfcData
+
+    fun updateNfcData(data: String) {
+        _nfcData.value = data
     }
-    val text: LiveData<String> = _text
 }
